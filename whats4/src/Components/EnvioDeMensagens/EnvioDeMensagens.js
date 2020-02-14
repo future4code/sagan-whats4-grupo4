52e2d0ccc0e352b9b68f19a45405a2029b7c2fb1
Leonardo Crispim <leonardo.crispim@outlook.com.br>
2020-02-14 15:10:27 -0300

    tudo completo e bonito

--- whats4/src/Components/EnvioDeMensagens/EnvioDeMensagens.js
+++ whats4/src/Components/EnvioDeMensagens/EnvioDeMensagens.js
@@ -4,10 +4,24 @@ import PropTypes from 'prop-types'
 
 const EnvioDeMensagensContainer = styled.div`
     display: flex;
+    height: 50px;
 `
 
 const StyledInput = styled.input`
     width: ${ props => props.comprimento};
+    height: 20px;
+    border-radius: 8px;
+    padding: 5px;
+    margin: 5px;
+    border: 0px transparent;
+`
+
+const StyledButton = styled.button`
+    height: 30px;
+    border-radius: 8px;
+    border: 0px transparent;
+    padding: 5px;
+    margin: 5px;
 `
 
 class EnvioDeMensagens extends React.Component {
@@ -33,17 +47,16 @@ class EnvioDeMensagens extends React.Component {
     }
 
     criarMsg = () => {
-        this.props.onCriaMensagem({
-            usuario: this.state.usuario + ":",
-            msgusuario: this.state.msgusuario
-        })
-        this.setState({
-            msgusuario: ""
-        })
-    }
-
-    aoApertarEnter = (event) => {
-        if(event.keyCode === 13){
+        if(this.state.usuario === ""){
+            this.props.onCriaMensagem({
+                usuario: this.state.usuario,
+                msgusuario: this.state.msgusuario
+            })
+            this.setState({
+                msgusuario: ""
+            })
+        }
+        else{
             this.props.onCriaMensagem({
                 usuario: this.state.usuario + ":",
                 msgusuario: this.state.msgusuario
@@ -54,12 +67,35 @@ class EnvioDeMensagens extends React.Component {
         }
     }
 
+    aoApertarEnter = (event) => {
+        if(event.keyCode === 13){
+            if(this.state.usuario === ""){
+                this.props.onCriaMensagem({
+                    usuario: this.state.usuario,
+                    msgusuario: this.state.msgusuario
+                })
+                this.setState({
+                    msgusuario: ""
+                })
+            }
+            else{
+                this.props.onCriaMensagem({
+                    usuario: this.state.usuario + ":",
+                    msgusuario: this.state.msgusuario
+                })
+                this.setState({
+                    msgusuario: ""
+                })
+            }
+        }
+    }
+
     render() {
         return (
             <EnvioDeMensagensContainer>
                 <StyledInput comprimento="7vw" type="text" placeholder="Usuario" value={this.props.usuario} onChange={this.onChangeUsuario}></StyledInput>
                 <StyledInput comprimento="48vw" type="text" placeholder="Mensagem" value={this.state.msgusuario} onChange={this.onChangeMSGUsuario} onKeyDown={this.aoApertarEnter}></StyledInput>
-                <button onClick={this.criarMsg}>Enviar</button>
+                <StyledButton onClick={this.criarMsg}>Enviar</StyledButton>
             </EnvioDeMensagensContainer>
         )
     }
